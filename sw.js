f38665a2f7c087e1e92a041b854fbe4943a321dd
boooleh <amanda.bohlin@outlook.com>
2026-04-04 22:12:22 +0200

    Enhance service worker caching strategy
    Update fetch event to cache responses for future use.

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
 const CACHE = 'todo-v1';
-const FILES = ['/todo-app/', '/todo-app/index.html', '/todo-app/manifest.json', '/todo-app/Minimalist_to-do_app_icon.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(response => {
+      return caches.open(CACHE).then(cache => {
+        cache.put(e.request, response.clone());
+        return response;
+      });
+    }))
+  );
 });
